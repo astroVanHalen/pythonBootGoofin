f02b676a559a361ba5216524db8699b4c88aac5c
astroVanHalen <haz13.tuta.io>
2023-03-13 22:57:56 -0400

    Testing done.  Code is ready for grading.

--- Lab8_ZAH.py
+++ Lab8_ZAH.py
@@ -13,13 +13,17 @@ def get_state_code(radio_sign):
    This function looks through the call sign for any character that can be converted to an integer.
    This will tell us the state code identifier.
    '''
-   for i in radio_sign:
-       isInt = True
-       try:
-          state_code = int(i)
-       except ValueError:
-          pass
-   return state_code
+   try:
+      for i in radio_sign:
+          isInt = True
+          try:
+             state_code = int(i)
+          except ValueError:
+             pass
+      return state_code
+   except:
+      print('Invalid call sign.  Try again.')
+      exit()
 
 
 def get_state(state_code):
@@ -54,32 +58,68 @@ def get_license(radio_sign, state_code):
    Then, by checking the length of the prefix and suffix, and the letter combinations,
    a license class is determined.
    '''
-   slice_index = radio_sign.index(state_code)
-   prefix = radio_sign[:slice_index]
-   suffix = radio_sign[(slice_index + 1):]
+   
+   slice_index = radio_sign.index(state_code)                                             # these three variables slice the call sign
+   prefix = radio_sign[:slice_index]                                                      # into state code, prefix and suffix
+   suffix = radio_sign[(slice_index + 1):]                                                # 
+
+
    if len(prefix) == 2 and len(suffix) == 3:
-      radio_class = "Group D"
+      if prefix[0] == 'k' or prefix[0] == 'w':
+         radio_class = "Novice, Club and Military Recreations Station"
+      else:
+         print('\nInvalid call sign.  Try again.')
+         exit()
 
    elif len(prefix) == 1 and len(suffix) == 3:
-      radio_class = "Group C"
+      if prefix[0] == 'k' or prefix[0] == 'n' or prefix[0] == 'w':
+         radio_class = "General, Technician, and Technician Plus Classes"
+      else:
+         print('\nInvalid call sign.  Try again.')
+         exit()
+
 
    elif len(prefix) == 2 and len(suffix) == 2:
-      if prefix[0] == "A":
-         radio_class = "Group A"
+      if prefix[0] == "a":
+         radio_class = "Amateur Extra Class"
       else:
-         radio_class = "Group B"
+         if prefix[0] == 'a' or prefix[0] == 'k' or prefix[0] == 'n' or prefix[0] == 'w':   #  This whole section of if elif else statements
+            radio_class = "Advanced Class"                                                  #  uses conditional statements based off of the
+         else:                                                                              #  fcc website to determine which license
+            print('\nInvalid call sign.  Try again.')                                       #  was ordered.  The rules are based from
+            exit()                                                                          #  prefix and suffix length, and also what the
+                                                                                            #  first letter of the call sign is.
+   elif len(prefix) == 2 and len(suffix) == 1:                                              #
+         if prefix[0] == 'a' or prefix[0] == 'n' or prefix[0] == 'k' or prefix[0] == 'w':   #   
+            radio_class = "Amateur Extra Class"
+         else:
+            print('\nInvalid call sign.  Try again.')
+            exit()
+
+   elif len(prefix) == 1 and len(suffix) == 2:
+         if prefix[0] == 'n' or prefix[0] == 'k' or prefix[0] == 'w':
+            radio_class = "Amateur Extra Class"
+         else:
+            print('\nInvalid call sign.  Try again.')
+            exit()
+
    else:
-         radio_class = "Group A"
+            print('\nInvalid call sign.  Try again.')
+            exit()
+            
    return radio_class
 
 
-
+######################################################################################
+# This next section declares variables and calls the functions.
+######################################################################################
 user_radio = input('\nEnter your call sign: ')
+user_radio = user_radio.lower()
 radio_sign = list(user_radio)
 state_code = str(get_state_code(radio_sign))
 state_list = get_state(state_code)
 state =  ', '.join(state_list)
 radio_class = get_license(radio_sign, state_code)
 
-print(f'\nLicense Class: {radio_class}\nState(s) Issued: {state}')
+print(f'\nLicense Class: {radio_class} \nState(s) Issued: {state}')
 ######################################################################################
